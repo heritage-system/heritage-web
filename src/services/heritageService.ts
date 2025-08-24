a8a16e682ae106f8680166f42c22f39b674331e4
admin <admin@LAPTOP-56OHGFRG>
2025-08-24 15:26:56 +0700

    refactor commit

---
+++ src/services/heritageService.ts
@@ -0,0 +1,14 @@
+import { ApiResponse } from "../types/apiResonse";
+import { UserCreationRequest, UserCreationResponse } from "../types/user";
+import { API_URL } from "../utils/baseUrl";
+import { fetchInterceptor } from "../utils/interceptor";
+
+export const searchHeritage = async (data: UserCreationRequest): Promise<ApiResponse<UserCreationResponse>> => {
+    const response = await fetchInterceptor(`${API_URL}/api/v1/users`, {
+        method: "POST",
+        body: JSON.stringify(data)
+    })
+
+    //const result: ApiResponse<UserCreationResponse> = await response.json();
+    return response;
+} 
