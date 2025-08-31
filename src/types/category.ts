a1bfac3197b71da11b5b4c5819e91c3ac8c2556d
viet81918 <viet81918@gmail.com>
2025-08-31 19:59:21 +0700

    vietnade: complete admin CRUD tag category 31/8/25

--- src/types/category.ts
+++ src/types/category.ts
@@ -1,5 +1,32 @@
 export interface Category {
-    id: number
-    name: string;
-    description: string;   
+  id: number;
+  name: string;
+  description: string;
+  nameUnsigned: string;
+  descriptionUnsigned: string;
+  createdBy: string;
+  createdAt: string; // or Date if you parse it
+  updatedAt: string; // or Date if you parse it
+  count: number; // linked heritage count
 }
+
+export type SortBy = "NAMEASC" | "NAMEDESC";
+
+export interface CategorySearchRequest {
+  keyword?: string;
+  sortBy?: SortBy;
+  page?: number;
+  pageSize?: number;
+}
+
+  export interface CategorySearchResponse {
+    id: number;
+    name: string;
+    description: string;
+    nameUnsigned: string;
+    descriptionUnsigned: string;
+    createdBy: string;
+    createdAt: string;
+    updatedAt: string;
+    count: number;
+  }
--- src/types/tag.ts
+++ src/types/tag.ts
@@ -1,5 +1,26 @@
+// src/types/tag.ts
+
 export interface Tag {
-    id: number
-    name: string;
-   
+  id: number;
+  name: string;
+  nameUnsigned: string;
+  createdBy: string;
+  createdAt: string; // ISO string
+  updatedAt: string; // ISO string
+}
+
+export interface TagSearchRequest {
+  keyword?: string;
+  page?: number;
+  pageSize?: number;
+}
+
+export interface TagSearchResponse {
+  id: number;
+  name: string;
+  nameUnsigned: string;
+  createdBy: string;
+  createdAt: string;
+  updatedAt: string;
+  count: number; // number of linked heritages
 }
