3061e9b02d2b2d56caf499cfdceda231f26aee71
admin <admin@LAPTOP-56OHGFRG>
2025-09-06 15:53:31 +0700

    commit

---
+++ src/types/contribution.ts
@@ -0,0 +1,14 @@
+import { ContributionStatus} from "./enum";
+export interface ContributionCreateRequest {
+  title: string,
+  content: string,
+  mediaUrl: string,
+}
+export interface ContributionCreateResponse {
+    contributionId: number,
+    contributorId: number,
+    title: string,
+    content: string,
+    price: number,
+    status: ContributionStatus,
+}
