82b208c23809a07ff2199335154f03e7ae5eaee9
admin <admin@LAPTOP-56OHGFRG>
2025-12-12 22:52:52 +0700

    refactor code

--- src/services/AIpredictService.ts
+++ src/services/AIpredictService.ts
@@ -6,10 +6,28 @@ import { PageResponse } from "../types/pageResponse";
 import { AI_API_URL,API_URL } from "../utils/baseUrl";
 import { fetchInterceptor } from "../utils/interceptor";
 
+// export const predictHeritage = async (
+//   file: File,
+//   params?: { top_k?: number; results?: number; threshold?: number }
+// ): Promise<ApiResponse<PredictApiPayload>> => {
+//   const formData = new FormData();
+//   formData.append("file", file);
+
+//   const query = new URLSearchParams();
+//   if (params?.top_k) query.append("top_k", params.top_k.toString());
+//   if (params?.results) query.append("results", params.results.toString());
+//   if (params?.threshold) query.append("threshold", params.threshold.toString());
+
+//   return await fetchInterceptor<PredictApiPayload>(
+//     `${AI_API_URL}/predict?${query.toString()}`,
+//     { method: "POST", body: formData }
+//   );
+// };
+
 export const predictHeritage = async (
   file: File,
   params?: { top_k?: number; results?: number; threshold?: number }
-): Promise<ApiResponse<PredictApiPayload>> => {
+) => {
   const formData = new FormData();
   formData.append("file", file);
 
@@ -19,10 +37,14 @@ export const predictHeritage = async (
   if (params?.threshold) query.append("threshold", params.threshold.toString());
 
   return await fetchInterceptor<PredictApiPayload>(
-    `${AI_API_URL}/predict?${query.toString()}`,
-    { method: "POST", body: formData }
+    `${API_URL}/api/proxy/predict?${query.toString()}`,
+    {
+      method: "POST",
+      body: formData,
+    }
   );
 };
+
 
 export class PredictJsonService {
   /**
