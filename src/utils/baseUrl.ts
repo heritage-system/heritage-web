4cd44f8f986a8ca872028fac89a291d329fa347e
admin <admin@LAPTOP-56OHGFRG>
2025-12-13 17:03:06 +0700

    refactor code

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,9 +1,9 @@
-//export const API_URL = 'https://cultural-heritage-system.runasp.net'; 
+export const API_URL = 'https://cultural-heritage-system.runasp.net'; 
 
-export const API_URL = 'https://localhost:7166'; 
+//export const API_URL = 'https://localhost:7166'; 
 
-export const FRONTEND_URL = 'http://localhost:3000'; 
+//export const FRONTEND_URL = 'http://localhost:3000'; 
 
-//export const FRONTEND_URL = 'https://heritage-web-ashy.vercel.app';
+export const FRONTEND_URL = 'https://heritage-web-ashy.vercel.app';
 
 export const AI_API_URL = 'http://135.171.217.70'; 
